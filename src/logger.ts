38719b9737cc7753e7a3464b00b3e9cca9fb8963
NaeCqde <50701386+NaeCqde@users.noreply.github.com>
2022-10-13 22:53:24 +0900

    Delete src directory

--- src/logger.ts
+++
@@ -1,7 +0,0 @@
-export async function log(message: string) {
-	process.stdout.write(`[${new Date().toLocaleString()}] [LOG]\t${message}\n`);
-}
-
-export async function error(message: string | Error) {
-	process.stderr.write(`\x1b[31m[${new Date().toLocaleString()}] [ERROR]\t${message instanceof Error ? `${message.name}\n\t${message.message}\n\t${message.stack}` : message}\x1b[0m\n`);
-}
